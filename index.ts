d7eda4e127a2cf345c2ef5257cc6f26869c5574d
Arunkumar S <arun14uit007@gmail.com>
2022-09-04 01:49:49 +0530

    Meta Key Inc

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata'
 import {Get,Post,Put,Patch,Delete,Trace} from './core/handler/Router/router';
 import controller from './core/decorators/controller.decorator';
 import { Method } from './core/utility/method.key';
+import {MetadataKeys} from './core/utility/meta-data.key';
 
 export const Routes ={
     Get,
@@ -11,7 +12,7 @@ export const Routes ={
     Delete,Patch,
     Trace
 }
-
+export const MetadataKey = MetadataKeys;
 export const Controller = controller;
 
 export interface IRouter {
