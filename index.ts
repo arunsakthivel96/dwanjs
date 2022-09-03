16e698fb75e9824b9ac3fecabe6b0b4d291e7cb0
Arunkumar S <arun14uit007@gmail.com>
2022-09-03 15:20:20 +0530

    Multiple Interface Added

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 
 import {Get,Post,Put,Patch,Delete,Trace} from './core/handler/Router/router';
 import controller from './core/decorators/controller.decorator';
+import { Method } from './core/utility/method.key';
 
 export const Routes ={
     Get,
@@ -12,3 +13,9 @@ export const Routes ={
 }
 
 export const Controller = controller;
+
+export interface IRouter {
+    method: Method;
+    path: string;
+    handlerName: string | symbol;
+  }
